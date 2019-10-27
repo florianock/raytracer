a64edf9ec41a6a9520fff5a47269e43f799dd961
Florian <florianock@users.noreply.github.com>
2019-10-27 13:36:45 +0100

    can correctly parse the book's feature files

--- src/main/kotlin/raytracer/Tuple.kt
+++ src/main/kotlin/raytracer/Tuple.kt
@@ -1,11 +1,28 @@
 package raytracer
 
-data class Tuple(val x: Double, val y: Double, val z: Double, val w: Double)
-
-fun point(x: Double, y: Double, z: Double): Tuple {
-    return Tuple(x, y, z, 1.0)
+data class Tuple(val x: Float, val y: Float, val z: Float, val w: Float) {
+    fun getType(): TupleType {
+        return when (w) {
+            1.0f -> TupleType.POINT
+            0.0f -> TupleType.VECTOR
+            -99f -> TupleType.COLOR
+            else -> TupleType.TUPLE
+        }
+    }
 }
 
-fun vector(x: Double, y: Double, z: Double): Tuple {
-    return Tuple(x, y, z, 0.0)
+fun point(x: Float, y: Float, z: Float): Tuple {
+    return Tuple(x, y, z, 1.0f)
+}
+
+fun vector(x: Float, y: Float, z: Float): Tuple {
+    return Tuple(x, y, z, 0.0f)
+}
+
+fun color(x: Float, y: Float, z: Float): Tuple {
+    return Tuple(x, y, z, -99f)
+}
+
+enum class TupleType {
+    COLOR, POINT, TUPLE, VECTOR
 }
--- src/test/kotlin/raytracer/StepDefs.kt
+++ src/test/kotlin/raytracer/StepDefs.kt
@@ -1,46 +1,74 @@
 package raytracer
 
-import cucumber.api.PendingException
 import io.cucumber.java.en.Given
 import io.cucumber.java.en.Then
 import io.cucumber.java8.En
-import junit.framework.Assert.assertEquals
+import junit.framework.TestCase.*
 
 class StepDefs: En {
-    private lateinit var a: Tuple
+    private var world: MutableMap<String, Any> = mutableMapOf()
 
-    @Given("a <- tuple\\({double}, {double}, {double}, {double})")
-    fun initializeTupleA(x: Double, y: Double, z: Double, w: Double) {
-        a = Tuple(x, y, z, w)
+    @Given("^(\\w+) ← (\\w+)\\(([-+]?[0-9]*\\.?[0-9]+), ([-+]?[0-9]*\\.?[0-9]+), ([-+]?[0-9]*\\.?[0-9]+), ([-+]?[0-9]*\\.?[0-9]+)\\)$")
+    fun initializeTupleOf4(a: String, ctr: String, x: Float, y: Float, z: Float, w: Float) {
+        world[a] = Tuple(x, y, z, w)
     }
 
-    @Then("a.x = {double}")
-    fun tupleAFieldXIsX(x: Double){
+    @Given("^(\\w+) ← (\\w+)\\(([-+]?[0-9]*\\.?[0-9]+), ([-+]?[0-9]*\\.?[0-9]+), ([-+]?[0-9]*\\.?[0-9]+)\\)$")
+    fun initializeTupleOf3(a: String, ctr: String, x: Float, y: Float, z: Float) {
+        val obj = when (ctr) {
+            "point" -> point(x, y, z)
+            "vector" -> vector(x, y, z)
+            "color" -> color(x, y, z)
+            else -> {
+                throw IllegalArgumentException()
+            }
+        }
+
+        world[a] = obj
+    }
+
+    @Then("^(\\w+).x = ([-+]?[0-9]*\\.?[0-9]+)$")
+    fun tupleFieldXIsX(key: String, x: Float){
+        val a = world[key] as Tuple
         assertEquals(x, a.x)
     }
 
-    @Then("a.y = {double}")
-    fun tupleAFieldYIsY(y: Double){
+    @Then("^(\\w+).y = ([-+]?[0-9]*\\.?[0-9]+)$")
+    fun tupleFieldYIsY(key: String, y: Float){
+        val a = world[key] as Tuple
         assertEquals(y, a.y)
     }
 
-    @Then("a.z = {double}")
-    fun tupleAFieldZIsZ(z: Double){
+    @Then("^(\\w+).z = ([-+]?[0-9]*\\.?[0-9]+)$")
+    fun tupleFieldZIsZ(key: String, z: Float){
+        val a = world[key] as Tuple
         assertEquals(z, a.z)
     }
 
-    @Then("a.w = {double}")
-    fun tupleAFieldWIsW(w: Double){
+    @Then("^(\\w+).w = ([-+]?[0-9]*\\.?[0-9]+)$")
+    fun tupleFieldWIsW(key: String, w: Float){
+        val a = world[key] as Tuple
         assertEquals(w, a.w)
     }
 
-    @Then("a is a Point")
-    fun aIsAPoint() {
-        assertEquals(a.w, 1)
+    @Then("^(\\w+) is([ not]+us) a (\\w+)$")
+    fun testType(key: String, not: String, type: String) {
+        val a = world[key] as Tuple
+        when (not) {
+            "" -> assertEquals(type.toLowerCase(), a.getType().toString().toLowerCase())
+            " not" -> assertTrue(type.toLowerCase() != a.getType().toString().toLowerCase())
+        }
     }
-
-//    @Then("a.w = {double}")
-//    fun tupleAFieldWIsW(w: Double){
-//        a.w == w
+//
+//    @Then("^(\\w+) is a (\\w+)$")
+//    fun aIsAType(key: String, type: String) {
+//        val a = world[key] as Tuple
+//        assertEquals(type.toLowerCase(), a.getType().toString().toLowerCase())
 //    }
+
+    @Then("(\\w+) = tuple\\(([-+]?[0-9]*\\.?[0-9]+), ([-+]?[0-9]*\\.?[0-9]+), ([-+]?[0-9]*\\.?[0-9]+), ([-+]?[0-9]*\\.?[0-9]+)\\)")
+    fun pEqualsTuple(key: String, x: Float, y: Float, z: Float, w: Float) {
+        val p = world[key] as Tuple
+        assertEquals(Tuple(x, y, z, w), p)
+    }
 }
