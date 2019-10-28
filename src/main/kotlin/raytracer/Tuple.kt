5874a8cbaf4fe9a9c5d01025a368f05877ef62d4
Florian <florianock@users.noreply.github.com>
2019-10-28 22:18:02 +0100

    Finished Chapter 1, tuple, points and vectors

--- src/main/kotlin/raytracer/Tuple.kt
+++ src/main/kotlin/raytracer/Tuple.kt
@@ -1,26 +1,84 @@
 package raytracer
 
+import kotlin.math.sqrt
+
 data class Tuple(val x: Float, val y: Float, val z: Float, val w: Float) {
     fun getType(): TupleType {
         return when (w) {
             1.0f -> TupleType.POINT
             0.0f -> TupleType.VECTOR
-            -99f -> TupleType.COLOR
             else -> TupleType.TUPLE
         }
     }
+
+    operator fun plus(a: Tuple): Tuple {
+        return tuple(this.x + a.x, this.y + a.y, this.z + a.z, this.w + a.w)
+    }
+
+    operator fun minus(a: Tuple): Tuple {
+        return tuple(this.x - a.x, this.y - a.y, this.z - a.z, this.w - a.w)
+    }
+
+    operator fun unaryMinus(): Tuple {
+        return tuple(-this.x, -this.y, -this.z, -this.w)
+    }
+
+    operator fun times(multiplier: Float): Tuple {
+        return tuple(
+                this.x * multiplier,
+                this.y * multiplier,
+                this.z * multiplier,
+                this.w * multiplier
+        )
+    }
+
+    operator fun div(divider: Float): Tuple {
+        return tuple(
+                this.x / divider,
+                this.y / divider,
+                this.z / divider,
+                this.w / divider
+        )
+    }
+}
+
+fun tuple(x: Float, y: Float, z: Float, w: Float): Tuple {
+    return Tuple(x, y, z, w)
 }
 
 fun point(x: Float, y: Float, z: Float): Tuple {
-    return Tuple(x, y, z, 1.0f)
+    return tuple(x, y, z, 1.0f)
 }
 
 fun vector(x: Float, y: Float, z: Float): Tuple {
-    return Tuple(x, y, z, 0.0f)
+    return tuple(x, y, z, 0.0f)
 }
 
 fun color(x: Float, y: Float, z: Float): Tuple {
-    return Tuple(x, y, z, -99f)
+    return tuple(x, y, z, -99f)
+}
+
+// Think Pythagoras
+fun magnitude(v: Tuple): Float {
+    return sqrt(v.x*v.x + v.y*v.y + v.z*v.z + v.w*v.w)
+}
+
+fun normalize(v: Tuple): Tuple {
+    val m = magnitude(v)
+    return vector(v.x / m, v.y / m, v.z / m)
+}
+
+fun dot(a:Tuple, b: Tuple): Float {
+    return  a.x * b.x +
+            a.y * b.y +
+            a.z * b.z +
+            a.w * b.w;
+}
+
+fun cross(a: Tuple, b: Tuple): Tuple {
+    return vector(  a.y * b.z - a.z * b.y,
+                    a.z * b.x - a.x * b.z,
+                    a.x * b.y - a.y * b.x)
 }
 
 enum class TupleType {
--- src/test/kotlin/raytracer/StepDefs.kt
+++ src/test/kotlin/raytracer/StepDefs.kt
@@ -2,29 +2,37 @@ package raytracer
 
 import io.cucumber.java.en.Given
 import io.cucumber.java.en.Then
+import io.cucumber.java.en.When
 import io.cucumber.java8.En
 import junit.framework.TestCase.*
+import kotlin.math.abs
+import kotlin.math.sqrt
 
 class StepDefs: En {
     private var world: MutableMap<String, Any> = mutableMapOf()
 
-    @Given("^(\\w+) ← (\\w+)\\(([-+]?[0-9]*\\.?[0-9]+), ([-+]?[0-9]*\\.?[0-9]+), ([-+]?[0-9]*\\.?[0-9]+), ([-+]?[0-9]*\\.?[0-9]+)\\)$")
-    fun initializeTupleOf4(a: String, ctr: String, x: Float, y: Float, z: Float, w: Float) {
-        world[a] = Tuple(x, y, z, w)
+    @Given("^(\\w+) ← tuple\\(([-+]?\\d*\\.?\\d+), ([-+]?\\d*\\.?\\d+), ([-+]?\\d*\\.?\\d+), ([-+]?\\d*\\.?\\d+)\\)$")
+    fun initializeTupleOf4(a: String, x: Float, y: Float, z: Float, w: Float) {
+        world[a] = tuple(x, y, z, w)
     }
 
-    @Given("^(\\w+) ← (\\w+)\\(([-+]?[0-9]*\\.?[0-9]+), ([-+]?[0-9]*\\.?[0-9]+), ([-+]?[0-9]*\\.?[0-9]+)\\)$")
+    @Given("^(\\w+) ← (\\w+)\\(([-+]?\\d*\\.?\\d+), ([-+]?\\d*\\.?\\d+), ([-+]?\\d*\\.?\\d+)\\)$")
     fun initializeTupleOf3(a: String, ctr: String, x: Float, y: Float, z: Float) {
         val obj = when (ctr) {
             "point" -> point(x, y, z)
             "vector" -> vector(x, y, z)
-            "color" -> color(x, y, z)
+//            "color" -> color(x, y, z)
             else -> {
                 throw IllegalArgumentException()
             }
         }
-
         world[a] = obj
+    }
+
+    @When("norm ← normalize\\(v)")
+    fun normIsNormalizeV() {
+        val v = world["v"] as Tuple
+        world["norm"] = normalize(v)
     }
 
     @Then("^(\\w+).x = ([-+]?[0-9]*\\.?[0-9]+)$")
@@ -51,24 +59,116 @@ class StepDefs: En {
         assertEquals(w, a.w)
     }
 
-    @Then("^(\\w+) is([ not]+us) a (\\w+)$")
-    fun testType(key: String, not: String, type: String) {
+    @Then("^(\\w+) is( | not )a (\\w+)$")
+    fun testType(key: String, mod: String, type: String) {
         val a = world[key] as Tuple
-        when (not) {
-            "" -> assertEquals(type.toLowerCase(), a.getType().toString().toLowerCase())
-            " not" -> assertTrue(type.toLowerCase() != a.getType().toString().toLowerCase())
+        when (mod) {
+            " " -> assertEquals(type.toLowerCase(), a.getType().toString().toLowerCase())
+            " not " -> assertTrue(type.toLowerCase() != a.getType().toString().toLowerCase())
+            else -> throw IllegalArgumentException()
         }
     }
-//
-//    @Then("^(\\w+) is a (\\w+)$")
-//    fun aIsAType(key: String, type: String) {
-//        val a = world[key] as Tuple
-//        assertEquals(type.toLowerCase(), a.getType().toString().toLowerCase())
-//    }
 
-    @Then("(\\w+) = tuple\\(([-+]?[0-9]*\\.?[0-9]+), ([-+]?[0-9]*\\.?[0-9]+), ([-+]?[0-9]*\\.?[0-9]+), ([-+]?[0-9]*\\.?[0-9]+)\\)")
-    fun pEqualsTuple(key: String, x: Float, y: Float, z: Float, w: Float) {
-        val p = world[key] as Tuple
-        assertEquals(Tuple(x, y, z, w), p)
+    @Then("([-]?)(\\w+) = tuple\\(([-+]?[0-9]*\\.?[0-9]+), ([-+]?[0-9]*\\.?[0-9]+), ([-+]?[0-9]*\\.?[0-9]+), ([-+]?[0-9]*\\.?[0-9]+)\\)")
+    fun keyEqualsTuple(neg: String, key: String, x: Float, y: Float, z: Float, w: Float) {
+        val t = world[key] as Tuple
+        if (neg == "-") {
+            assertEquals(Tuple(x, y, z, w), -t)
+        } else {
+            assertEquals(Tuple(x, y, z, w), t)
+        }
+    }
+
+    @Then("^(\\w+) ([+-]) (\\w+) = tuple\\(([-+]?\\d*\\.?\\d+), ([-+]?\\d*\\.?\\d+), ([-+]?\\d*\\.?\\d+), ([-+]?\\d*\\.?\\d+)\\)$")
+    fun tuplePlusTuple(tupleA: String, op: String, tupleB: String, x: Float, y: Float, z: Float, w: Float) {
+        val a = world[tupleA] as Tuple
+        val b = world[tupleB] as Tuple
+        when (op) {
+            "+" -> assertEquals(tuple(x, y, z, w), a + b)
+            else -> throw IllegalArgumentException()
+        }
+    }
+
+    @Then("^(\\w+) ([-]) (\\w+) = (\\w+)\\(([-+]?\\d*\\.?\\d+), ([-+]?\\d*\\.?\\d+), ([-+]?\\d*\\.?\\d+)\\)$")
+    fun tupleMinusTuple(pointA: String, op: String, pointB: String, ctr: String, x: Float, y: Float, z: Float) {
+        val a = world[pointA] as Tuple
+        val b = world[pointB] as Tuple
+
+        val expected: Tuple = when (ctr) {
+            "vector" -> vector(x, y, z)
+            "point" -> point(x, y, z)
+            else -> throw IllegalArgumentException()
+        }
+
+        when (op) {
+            "-" -> assertEquals(expected, a - b)
+            else -> throw IllegalArgumentException()
+        }
+    }
+
+    @Then("^a ([*/]) ([-+]?\\d*\\.?\\d+) = tuple\\(([-+]?\\d*\\.?\\d+), ([-+]?\\d*\\.?\\d+), ([-+]?\\d*\\.?\\d+), ([-+]?\\d*\\.?\\d+)\\)$")
+    fun scalarMultiplyingATuple(op: String, multiplier: Float, x: Float, y: Float, z: Float, w: Float) {
+        val a = world["a"] as Tuple
+        val result = when (op) {
+            "*" -> a * multiplier
+            "/" -> a / multiplier
+            else -> throw IllegalArgumentException()
+        }
+        assertEquals(tuple(x, y, z, w), result)
+    }
+
+    @Then("^magnitude\\(v\\) =( | √)([-+]?\\d*\\.?\\d+)$")
+    fun magnitudeOfVEquals(sw: String, mag: Float) {
+        val v = world["v"] as Tuple
+        var m = mag
+        if (sw == " √") {
+            m = sqrt(mag)
+        }
+        assertEquals(m, magnitude(v))
+    }
+
+    @Then("^normalize\\(v\\) = vector\\(([-+]?\\d*\\.?\\d+), ([-+]?\\d*\\.?\\d+), ([-+]?\\d*\\.?\\d+)\\)$")
+    fun normalizeVector(x: Float, y: Float, z: Float) {
+        val v = world["v"] as Tuple
+        assertEquals(vector(x, y, z), normalize(v))
+    }
+
+    @Then("^normalize\\(v\\) = approximately vector\\(([-+]?\\d*\\.?\\d+), ([-+]?\\d*\\.?\\d+), ([-+]?\\d*\\.?\\d+)\\)$")
+    fun normalizeVectorEpsilon(x: Float, y: Float, z: Float) {
+        val v = world["v"] as Tuple
+        val expected = vector(x, y, z)
+        val result = normalize(v)
+        assertEqualsApprox(expected, result);
+    }
+
+    @Then("^magnitude\\(norm\\) = ([-+]?\\d*\\.?\\d+)$")
+    fun magnitudeNormalizedVector(m: Float) {
+        val norm = world["norm"] as Tuple
+        assertEqualsApprox(m, magnitude(norm))
+    }
+
+    @Then("^dot\\(a, b\\) = ([-+]?\\d*\\.?\\d+)$")
+    fun dotProductTest(dp: Float) {
+        val a = world["a"] as Tuple;
+        val b = world["b"] as Tuple;
+        assertEquals(dp, dot(a, b));
+    }
+
+    @Then("^cross\\((\\w+), (\\w+)\\) = vector\\(([-+]?\\d*\\.?\\d+), ([-+]?\\d*\\.?\\d+), ([-+]?\\d*\\.?\\d+)\\)$")
+    fun crossProductTest(a: String, b: String, x: Float, y: Float, z: Float) {
+        val c = world[a] as Tuple;
+        val d = world[b] as Tuple
+        assertEquals(vector(x, y, z), cross(c, d))
+    }
+
+    private fun assertEqualsApprox(a: Float, b: Float, eps: Float = 0.0001f) {
+        assertTrue(abs(a - b) < eps);
+    }
+
+    private fun assertEqualsApprox(a: Tuple, b: Tuple, eps: Float = 0.0001f) {
+        assertTrue(abs(a.x - b.x) < eps);
+        assertTrue(abs(a.y - b.y) < eps);
+        assertTrue(abs(a.z - b.z) < eps);
+        assertTrue(abs(a.w - b.w) < eps);
     }
 }
